89e17eaf4d25168ec347b331af471f6a59d6446e
Alex <Alex.Tsumibito@gmail.com>
2024-11-24 12:20:44 +0100

    Update Navi-form-landing.js
    reInit kwiga form added

--- Navi-form-landing.js
+++ Navi-form-landing.js
@@ -1,13 +1,23 @@
-
 document.addEventListener("DOMContentLoaded", function(){
  function isCookieModalVisible() {
    const cookieModal = document.querySelector('.cf_modal');
    return cookieModal && window.getComputedStyle(cookieModal).display !== 'none';
  }
 
+ function reInitKwigaForm() {
+   const modal = document.getElementById("contact-modal");
+   const oldScript = modal.querySelector('script[src*="kwiga.com"]');
+   if (oldScript) {
+     oldScript.remove();
+   }
+   const newScript = document.createElement('script');
+   newScript.type = 'module';
+   newScript.src = 'https://kwiga.com/build/js/kwiga-widget.js?t=1732351704000&uuid=8a095c24-4119-47e0-bd17-1f107c313507&producer=https://kwiga.com/';
+   modal.appendChild(newScript);
+ }
+
  function openModal(){
    if (isCookieModalVisible()) return;
-
    var modalBg = document.getElementById("contact_modal_bg"),
        modal = document.getElementById("contact-modal");
    
@@ -16,7 +26,10 @@ document.addEventListener("DOMContentLoaded", function(){
    modalBg.style.display = "flex";
    modalBg.style.opacity = "0";
    modalBg.style.transition = "opacity 0.5s ease";
-   setTimeout(function(){ modalBg.style.opacity = "1" }, 10);
+   setTimeout(function(){ 
+     modalBg.style.opacity = "1";
+     reInitKwigaForm(); // Переинициализация формы при открытии
+   }, 10);
    
    modal.style.transform = "translateY(100%)";
    modal.style.transition = "transform 0.5s ease";
@@ -50,6 +63,3 @@ document.addEventListener("DOMContentLoaded", function(){
    button.addEventListener("click", closeModal);
  });
 });
-
-
-
