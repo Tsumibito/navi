793e8e8847930968bcd8a8a327135890b5538b51
Alex <Alex.Tsumibito@gmail.com>
2024-11-24 13:26:06 +0100

    Create Navi-form-landing4.js

---
+++ Navi-form-landing4.js
@@ -0,0 +1,56 @@
+// v4-1
+document.addEventListener("DOMContentLoaded", function(){
+ function isCookieModalVisible() {
+   const cookieModal = document.querySelector('.cf_modal');
+   return cookieModal && window.getComputedStyle(cookieModal).display !== 'none';
+ }
+
+ function openModal(){
+   if (isCookieModalVisible()) return;
+   var modalBg = document.getElementById("contact_modal_bg"),
+       modal = document.getElementById("contact-modal");
+   
+   if(!modalBg || !modal) return;
+   
+   modalBg.style.display = "flex";
+   modalBg.style.opacity = "0";
+   modalBg.style.transition = "opacity 0.5s ease";
+   setTimeout(function(){ modalBg.style.opacity = "1" }, 10);
+   
+   modal.style.transform = "translateY(100%)";
+   modal.style.transition = "transform 0.5s ease";
+   setTimeout(function(){ modal.style.transform = "translateY(0)" }, 10);
+   
+   // Предотвращаем прокрутку страницы
+   document.body.style.overflow = 'hidden';
+ }
+
+ function closeModal(){
+   var modalBg = document.getElementById("contact_modal_bg"),
+       modal = document.getElementById("contact-modal");
+       
+   if(!modalBg || !modal) return;
+   
+   modalBg.style.opacity = "0";
+   modal.style.transform = "translateY(100%)";
+   
+   setTimeout(function(){ 
+     modalBg.style.display = "none";
+     // Восстанавливаем прокрутку
+     document.body.style.overflow = '';
+   }, 500);
+ }
+
+ var openButtons = document.querySelectorAll('[Role="formbutton"]');
+ openButtons.forEach(function(button){
+   button.addEventListener("click", function(e) {
+     e.preventDefault();
+     openModal();
+   });
+ });
+
+ var closeButtons = document.querySelectorAll('[Role="formclosebutton"]');
+ closeButtons.forEach(function(button){
+   button.addEventListener("click", closeModal);
+ });
+});
